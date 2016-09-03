ddf72661fdd8c364a744baab7ac5b61c3296bd1d
Mark van de Korput <dr.theman@gmail.com>
2016-09-04 01:17:39 +0200

    initial implementation + test scripts

---
+++ test_master.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python2
+try:
+    from omxplayer import OMXPlayer
+except ImportError:
+    print("\n!! Could not import OMXPlayer, see https://github.com/willprice/python-omxplayer-wrapper for install instructions !!\n")
+
+from pyOmxSync.broadcaster import Broadcaster
+import sys
+
+if len(sys.argv) < 2:
+    print('No video specified\nUsage: python test_master.py path/to/video.mp4\n')
+    sys.exit(1)
+
+try:
+    print("Load player")
+    player = OMXPlayer(sys.argv[1])
+
+    print("Load broadcaster")
+    broadcaster = Broadcaster(player, {'verbose': True})
+    broadcaster.setup()
+
+    print("Start playing")
+    player.play()
+
+    while player.playback_status() != "Stopped":
+        broadcaster.update()
+
+    print("Stop player")
+    player.stop()
+
+    print("Exit")
+except Exception as err:
+    print("An error occured:")
+    print(err)
