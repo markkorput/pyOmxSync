2e36f43c95c3c9d6bf20e8ef1031b5ea89ab5e05
Mark van de Korput <dr.theman@gmail.com>
2016-09-04 21:37:06 +0200

    bump to version 0.1.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='omxsync',
-      version='0.1',
+      version='0.1.1',
       description='Master/slave syncing of omxplayer instances over network',
       url='http://github.com/markkorput/pyOmxSync',
       author='Mark van de Korput',
